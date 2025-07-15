8f44403d78aee854b09527d9d74463af95a3a69b
sergioarango25 <sergioarangogarcia8@gmail.com>
2025-07-15 00:21:57 -0500

    Pagina web

---
+++ index.js
@@ -0,0 +1,16 @@
+function pagina(){
+    window.location.href = "menu.html"
+}
+
+function salida(){
+    window.location.href = "index.html"
+}
+
+function domicilio(){
+    window.location.href = "domicilio.html"
+}
+// esto sirve para ir a otra pagina con js
+
+function salir(){
+    window.location.href = "menu.html"
+}
