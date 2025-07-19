9425dc4fff6e5089994dc1818af215e6786d4342
sergioarango25 <sergioarangogarcia8@gmail.com>
2025-07-19 01:08:57 -0500

    actualizacion de estilo y nuevas secciones

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ function salida(){
 function domicilio(){
     window.location.href = "domicilio.html"
 }
-// esto sirve para ir a otra pagina con js
 
-function salir(){
-    window.location.href = "menu.html"
+function ingresar(){
+    window.location.href = "hamburguesa.html"
+}
+
+function ingresar2(){
+    window.location.href = "perros.html"
+}
+
+function ingresar3(){
+    window.location.href = "pizza.html"
+}
+
+function ingresar4(){
+    window.location.href = "salchipapa.html"
 }
